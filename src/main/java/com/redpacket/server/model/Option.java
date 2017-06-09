a104be5d6df266d467a8daf7f2994aa56c872c48
liudonghua123 <liudonghua123@gmail.com>
2017-06-10 00:47:10 +0800

    backquote reserved keyword table name

--- src/main/java/com/redpacket/server/model/Option.java
+++ src/main/java/com/redpacket/server/model/Option.java
@@ -6,8 +6,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="\"option\"")
 public class Option implements Serializable {
 	
 	private static final long serialVersionUID = -747244113496826262L;
