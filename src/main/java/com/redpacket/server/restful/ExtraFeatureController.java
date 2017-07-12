d79b477a634d2d5d2a5e799abe5ceeda1e4fbcd3
liudonghua123 <liudonghua123@gmail.com>
2017-07-12 11:43:03 +0800

    add redirect feature

--- src/main/java/com/redpacket/server/restful/ExtraFeatureController.java
+++ src/main/java/com/redpacket/server/restful/ExtraFeatureController.java
@@ -1,20 +1,29 @@
 package com.redpacket.server.restful;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 
 import com.redpacket.server.common.GeneralResponse;
 import com.redpacket.server.service.ExtraFeatureService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.Authorization;
 
 @CrossOrigin
 @Api(tags = { "extra" })
@@ -34,6 +43,22 @@ public class ExtraFeatureController {
 		String cityString = extraFeatureService.updateLocation(openId, latitude, longitude);
 		return new GeneralResponse<String>(cityString != null ? GeneralResponse.SUCCESS : GeneralResponse.ERROR, cityString);
 
+	}
+
+	@RequestMapping(value = "redirect")
+	public ModelAndView redirect(HttpServletRequest request, HttpServletResponse response,
+			@RequestParam(name = "redirect_code", required = false, defaultValue = "301") int redirect_code,
+			@RequestParam(name = "url", required = false, defaultValue = "http://inthb.cn/redpacket-share.html") String url) {
+		HttpStatus redirectHttpStatus = (redirect_code == 301) ? HttpStatus.MOVED_PERMANENTLY : HttpStatus.FOUND;
+		request.setAttribute(View.RESPONSE_STATUS_ATTRIBUTE, redirectHttpStatus);
+		return new ModelAndView("redirect:" + url);
+	}
+
+	@RequestMapping(value = "redirect_refresh")
+	public ModelAndView redirect_refresh(Model model,
+			@RequestParam(name = "url", required = false, defaultValue = "http://inthb.cn/redpacket-share.html") String url) {
+		model.addAttribute("url", String.format("0;URL='%s'", url));
+		return new ModelAndView("redpacket-redirect");
 	}
 
 }
